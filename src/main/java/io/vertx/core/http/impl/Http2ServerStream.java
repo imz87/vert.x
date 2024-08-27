fd6f7de33d733795c9baf58aac49d668a1ab8cb2
imz87 <zolfaghari19@gmail.com>
2024-08-27 13:31:37 +0330

    feat: remove generic headers to simplify

--- src/main/java/io/vertx/core/http/impl/Http2ServerStream.java
+++ src/main/java/io/vertx/core/http/impl/Http2ServerStream.java
@@ -29,6 +29,7 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.StreamPriorityBase;
 import io.vertx.core.http.StreamPriority;
 import io.vertx.core.http.impl.headers.Http2HeadersAdaptor;
+import io.vertx.core.http.impl.headers.VertxDefaultHttpHeaders;
 import io.vertx.core.impl.ContextInternal;
 import io.vertx.core.net.HostAndPort;
 import io.vertx.core.spi.metrics.HttpServerMetrics;
@@ -146,7 +147,7 @@ class Http2ServerStream extends VertxHttpStreamBase<Http2ServerConnection, Http2
   }
 
   @Override
-  void doWriteHeaders(Http2Headers headers, boolean end, boolean checkFlush, Handler<AsyncResult<Void>> handler) {
+  void doWriteHeaders(VertxDefaultHttpHeaders headers, boolean end, boolean checkFlush, Handler<AsyncResult<Void>> handler) {
     if (Metrics.METRICS_ENABLED && !end) {
       HttpServerMetrics metrics = conn.metrics();
       if (metrics != null) {
@@ -285,7 +286,7 @@ class Http2ServerStream extends VertxHttpStreamBase<Http2ServerConnection, Http2
     conn.handler.writeFrame(stream, type, flags, payload);
   }
   @Override
-  public void writeHeaders(Http2Headers headers, boolean end, StreamPriorityBase priority,
+  public void writeHeaders(VertxDefaultHttpHeaders headers, boolean end, StreamPriorityBase priority,
                            boolean checkFlush, FutureListener<Void> promise) {
     conn.handler.writeHeaders(stream, headers, end, priority.getDependency(), priority.getWeight(), priority.isExclusive(),
       checkFlush, promise);
