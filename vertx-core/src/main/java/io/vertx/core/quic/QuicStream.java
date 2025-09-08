d7bf12cb771de5f17e203cfa27ca0269e1a6dc75
Julien Viet <julien@julienviet.com>
2025-09-08 21:24:58 +0200

    Quic API and implementation - this is a work in progress.

---
+++ vertx-core/src/main/java/io/vertx/core/quic/QuicStream.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2011-2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.core.quic;
+
+import io.vertx.codegen.annotations.Nullable;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.Socket;
+import io.vertx.core.streams.ReadStream;
+import io.vertx.core.streams.WriteStream;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public interface QuicStream extends Socket {
+
+  QuicConnection connection();
+
+  @Override
+  QuicStream closeHandler(@Nullable Handler<Void> handler);
+
+  @Override
+  QuicStream shutdownHandler(@Nullable Handler<Void> handler);
+
+  @Override
+  QuicStream exceptionHandler(@Nullable Handler<Throwable> handler);
+
+  @Override
+  QuicStream handler(@Nullable Handler<Buffer> handler);
+
+  @Override
+  QuicStream pause();
+
+  @Override
+  QuicStream resume();
+
+  @Override
+  QuicStream fetch(long amount);
+
+  @Override
+  QuicStream endHandler(@Nullable Handler<Void> endHandler);
+
+  @Override
+  QuicStream setWriteQueueMaxSize(int maxSize);
+
+  @Override
+  QuicStream drainHandler(@Nullable Handler<Void> handler);
+}
---
+++ vertx-core/src/main/java/io/vertx/core/quic/impl/QuicEndpointImpl.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2011-2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.core.quic.impl;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.handler.codec.quic.QuicCodecBuilder;
+import io.vertx.core.Future;
+import io.vertx.core.internal.ContextInternal;
+import io.vertx.core.internal.PromiseInternal;
+import io.vertx.core.internal.VertxInternal;
+import io.vertx.core.internal.tls.SslContextManager;
+import io.vertx.core.internal.tls.SslContextProvider;
+import io.vertx.core.net.SSLEngineOptions;
+import io.vertx.core.net.SocketAddress;
+import io.vertx.core.quic.QuicEndpoint;
+import io.vertx.core.quic.QuicEndpointOptions;
+import io.vertx.core.quic.QuicOptions;
+import io.vertx.core.spi.tls.QuicSslContextFactory;
+import io.vertx.core.spi.tls.SslContextFactory;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public abstract class QuicEndpointImpl implements QuicEndpoint {
+
+  private final QuicEndpointOptions options;
+  private final SslContextManager manager;
+  protected final VertxInternal vertx;
+  private Channel channel;
+
+  public QuicEndpointImpl(VertxInternal vertx, QuicEndpointOptions options) {
+    this.options = options;
+    this.vertx = vertx;
+    this.manager = new SslContextManager(new SSLEngineOptions() {
+      @Override
+      public SSLEngineOptions copy() {
+        return this;
+      }
+      @Override
+      public SslContextFactory sslContextFactory() {
+        return new QuicSslContextFactory();
+      }
+    });
+  }
+
+  protected abstract QuicCodecBuilder<?> codecBuilder(ContextInternal context, SslContextProvider sslContextProvider);
+
+  protected abstract Future<SslContextProvider> createSslContextProvider(SslContextManager manager, ContextInternal context);
+
+  protected ChannelHandler channelHandler(ContextInternal context, SocketAddress bindAddr, SslContextProvider sslContextProvider) {
+    QuicCodecBuilder<?> codecBuilder = initQuicCodecBuilder(context, sslContextProvider);
+    return codecBuilder.build();
+  }
+
+  private Future<Channel> bind(ContextInternal context, SocketAddress bindAddr, SslContextProvider sslContextProvider) {
+    Bootstrap bootstrap = new Bootstrap()
+      .group(context.nettyEventLoop())
+      .channelFactory(vertx.transport().datagramChannelFactory());
+    InetSocketAddress addr = new InetSocketAddress(bindAddr.hostName(), bindAddr.port());
+    ChannelHandler handler = channelHandler(context, bindAddr, sslContextProvider);
+    bootstrap.handler(handler);
+    ChannelFuture channelFuture = bootstrap.bind(addr);
+    PromiseInternal<Void> p = context.promise();
+    channelFuture.addListener(p);
+    return p.future().map(v -> channelFuture.channel());
+  }
+
+  protected QuicCodecBuilder<?> initQuicCodecBuilder(ContextInternal context, SslContextProvider sslContextProvider) {
+    QuicCodecBuilder<?> codecBuilder = codecBuilder(context, sslContextProvider);
+    QuicOptions transportOptions = options.getTransportOptions();
+    if (transportOptions.getInitialMaxData() != null) {
+      codecBuilder.initialMaxData(transportOptions.getInitialMaxData());
+    }
+    if (transportOptions.getInitialMaxStreamDataBidirectionalRemote() != null) {
+      codecBuilder.initialMaxStreamDataBidirectionalLocal(transportOptions.getInitialMaxStreamDataBidirectionalRemote());
+    }
+    if (transportOptions.getInitialMaxStreamDataBidirectionalRemote() != null) {
+      codecBuilder.initialMaxStreamDataBidirectionalRemote(transportOptions.getInitialMaxStreamDataBidirectionalRemote());
+    }
+    if (transportOptions.getInitialMaxStreamsBidirectional() != null) {
+      codecBuilder.initialMaxStreamsBidirectional(transportOptions.getInitialMaxStreamsBidirectional());
+    }
+    if (transportOptions.getInitialMaxStreamsUnidirectional() != null) {
+      codecBuilder.initialMaxStreamsUnidirectional(transportOptions.getInitialMaxStreamsUnidirectional());
+    }
+    if (transportOptions.getInitialMaxStreamDataBidirectionalLocal() != null) {
+      codecBuilder.initialMaxStreamDataBidirectionalLocal(transportOptions.getInitialMaxStreamDataBidirectionalLocal());
+    }
+    if (transportOptions.getInitialMaxStreamDataUnidirectional() != null) {
+      codecBuilder.initialMaxStreamDataUnidirectional(transportOptions.getInitialMaxStreamDataUnidirectional());
+    }
+    if (transportOptions.getActiveMigration() != null) {
+      codecBuilder.activeMigration(transportOptions.getActiveMigration());
+    }
+    return codecBuilder;
+  }
+
+  protected void handleBind(Channel channel) {
+    this.channel = channel;
+  }
+
+  @Override
+  public Future<Void> bind(SocketAddress address) {
+    ContextInternal context = vertx.getOrCreateContext();
+    Future<SslContextProvider> f1 = createSslContextProvider(manager, context);
+    Future<Channel> f2 = f1.compose(sslContextProvider -> bind(context, address, sslContextProvider));
+    return f2.map(ch -> {
+      handleBind(ch);
+      return null;
+    });
+  }
+
+  @Override
+  public Future<Void> close() {
+    ContextInternal context = vertx.getOrCreateContext();
+    Channel ch = channel;
+    if (ch == null) {
+      return context.succeededFuture();
+    }
+    PromiseInternal<Void> promise = context.promise();
+    ch.close().addListener(promise);
+    return promise.future();
+  }
+}
