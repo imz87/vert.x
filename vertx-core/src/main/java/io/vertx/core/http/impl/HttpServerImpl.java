b62ed29c39c48752a649c5fae90b1253afd2e41a
imz87 <zolfaghari19@gmail.com>
2025-09-16 14:54:48 +0330

    refactor: ignore trafficShaping for now

--- vertx-core/src/main/java/io/vertx/core/http/impl/HttpServerImpl.java
+++ vertx-core/src/main/java/io/vertx/core/http/impl/HttpServerImpl.java
@@ -224,7 +224,7 @@ public class HttpServerImpl implements HttpServer, MetricsProvider {
         handler,
         exceptionHandler,
         soi.metric(),
-        server.getTrafficShapingHandler());
+        null);
       initializer.configurePipeline(soi.channel(), null, null);
     });
     tcpServer = server;
